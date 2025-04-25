138a6590826baaa34fc49d8c8373b12b1d0a7dc5
Foxx05 <theocolpaert8@gmail.com>
2025-04-25 12:41:30 +0200

    version mobile
    Avancement sur la v1 mobile (25/04)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,23 @@
-console.info('Hello world');
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.querySelector(".menu__btn");
+    const menu = document.querySelector(".menu");
+    const overlay = document.querySelector(".blur-overlay");
+    const links = document.querySelectorAll(".menu__link");
+    const body = document.body; // Ajout pour gérer la classe globale
+
+    function toggleMenu() {
+        body.classList.toggle("menu--open"); // Ajoute/Retire sur <body>
+        document.body.classList.toggle("no-scroll");
+    }
+
+    menuBtn.addEventListener("click", toggleMenu);
+    overlay.addEventListener("click", toggleMenu);
+    
+    links.forEach(link => {
+        link.addEventListener("click", toggleMenu);
+    });
+});
+
+
