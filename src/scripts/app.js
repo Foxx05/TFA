3cc4777cf9c52e8bf5fe3a8649b48a1e9c375736
Foxx05 <theocolpaert8@gmail.com>
2025-04-25 17:35:25 +0200

    Footer
    Début mise en place footer

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -25,3 +25,12 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+let backToTopBtn = document.querySelector('.backToTop');
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+backToTopBtn.addEventListener("click", scrollToTop);
+
+
